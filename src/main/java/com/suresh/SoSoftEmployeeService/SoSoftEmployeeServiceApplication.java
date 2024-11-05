181bab10e0bc61f4f1e299feb6f27cdbd30f2568
suresh chandra <sureshchmca786@gmail.com>
2024-11-05 21:22:50 +0530

    SoSoftEmployeeService

---
+++ src/main/java/com/suresh/SoSoftEmployeeService/SoSoftEmployeeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.suresh.SoSoftEmployeeService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoSoftEmployeeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoSoftEmployeeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suresh/SoSoftEmployeeService/SoSoftEmployeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suresh.SoSoftEmployeeService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoSoftEmployeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
